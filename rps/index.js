e0006c129e7c6e4f4193071b25ffbfea8a1066d9
anujpatel1998 <134542862+AnujPatel1998@users.noreply.github.com>
2024-04-19 21:22:20 +0530

    Update index.js

--- rps/index.js
+++ rps/index.js
@@ -78,8 +78,8 @@ function playGame(playerMove) {
   updateScoreElement();
   document.querySelector('.js-result').innerHTML = result;
   document.querySelector('.js-moves').innerHTML = `You
-<img src="rps/${playerMove}-emoji.png" class="move-icon">
-<img src="rps/${computerMove}-emoji.png" class="move-icon">
+<img src="${playerMove}-emoji.png" class="move-icon">
+<img src="${computerMove}-emoji.png" class="move-icon">
 Computer`;
 }
 function updateScoreElement() {
